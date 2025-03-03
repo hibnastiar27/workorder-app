d8de0b91206f4ffb04fdf9be849e2bdd9a6dd214
Nur Aria Hibnastiar <96623047+hibnastiar27@users.noreply.github.com>
2025-03-03 10:35:12 +0700

    Update homepage route to return JSON response with project details

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        "title" => "API Workorder [Coding Test]",
+        "owner" => [
+            "full_name" => "Nur Aria Hibnastiar",
+            "link_linkedin" => "https://www.linkedin.com/in/hibnastiar/",
+            "link_github" => "https://github.com/hibnastiar27",
+        ]
+    ]);
 });
