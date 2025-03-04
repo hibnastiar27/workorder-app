4e3ddae90a3e57ab4d1d42ac6d52e15629ac728e
Nur Aria Hibnastiar <96623047+hibnastiar27@users.noreply.github.com>
2025-03-04 16:11:54 +0700

    Add LoginPage component to integrate AuthLayout and LoginForm

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { LoginForm } from "@/components/login-form"
+import AuthLayout from "@/layouts/AuthLayout"
+
+const LoginPage: React.FC = () => {
+  return (
+    <AuthLayout>
+      <LoginForm />
+    </AuthLayout>
+  )
+}
+export default LoginPage;
