9ff1c7df35e790410fb88b7bd8cefdd6a0ea7443
Nur Aria Hibnastiar <96623047+hibnastiar27@users.noreply.github.com>
2025-03-05 17:21:24 +0700

    Comment out unused routes for workorder pages in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+// import { WorkorderPage, WorkorderDetailsPage, WorkorderReportPage } from "./pages/manage-workorder/index";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* <Route path="/" element={<WorkorderPage />} /> */}
+        {/* <Route path="/details" element={<WorkorderDetailsPage />} /> */}
+        {/* <Route path="/report" element={<WorkorderReportPage />} /> */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
